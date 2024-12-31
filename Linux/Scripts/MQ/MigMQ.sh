3bc1d7bcf96b3b80bff64652b5c650cc61702a34
SudhakarAnemu <sudhakar.anemu@gmail.com>
2024-12-31 16:08:32 -0500

    Update MigMQ.sh

--- Linux/Scripts/MQ/MigMQ.sh
+++ Linux/Scripts/MQ/MigMQ.sh
@@ -7,46 +7,46 @@ qmgr=$1
 brk=$2
 v12ver=$3
 
+LOG=$qmgr.QMGRMig.log.$(date +%Y-%m-%d_%H-%M-%S)
 
+echo -e "Name of the qmgr : $1, Broker : $2, V12MQ : $3 -------------------------------------------" >> $LOG
 
-echo -e "Name of the qmgr : $1, Broker : $2, V12MQ : $3 -------------------------------------------"
-
-echo -e "Setting v10 profile -------------------------------------------"
+echo -e "Setting v10 profile -------------------------------------------" >> $LOG
 #setwmb 10
 . /WebSphere/scripts/middleware/wmbprofile 10
 
-/WebSphere/scripts/middleware/qmgrSuspend.sh $qmgr
+/WebSphere/scripts/middleware/qmgrSuspend.sh $qmgr >> $LOG
 
-echo -e "mqsilist of the brk"
-mqsilist | grep $brk 
+echo -e "mqsilist of the brk" >> $LOG
+mqsilist | grep $brk  >> $LOG
 
-echo -e "dspmq of the qmgr"
-dspmq -o all | grep $qmgr
+echo -e "dspmq of the qmgr" >> $LOG
+dspmq -o all | grep $qmgr >> $LOG
 
-echo -e "Health of the qmgr : -------------------------------------------"
-/WebSphere/scripts/middleware/mqhealth.sh | grep $qmgr
+echo -e "Health of the qmgr : -------------------------------------------" >> $LOG
+/WebSphere/scripts/middleware/mqhealth.sh | grep $qmgr >> $LOG
 
-echo -e "Stopping the Broker -------------------------------------------"
-perl /WebSphere/scripts/middleware/wmbRestart.pl $brk stop
+echo -e "Stopping the Broker -------------------------------------------" >> $LOG
+perl /WebSphere/scripts/middleware/wmbRestart.pl $brk stop >> $LOG
 
-echo -e "Stopping the QMGR -------------------------------------------"
-endmqm -i $qmgr
+echo -e "Stopping the QMGR -------------------------------------------" >> $LOG
+endmqm -i $qmgr >> $LOG
 
-echo -e "Process of Broker-------------------------------------------"
-ps -ef | grep $brk
+echo -e "Process of Broker-------------------------------------------" >> $LOG
+ps -ef | grep $brk >> $LOG
 
-echo -e "Process of qmgr-------------------------------------------"
-ps -ef | grep $qmgr
+echo -e "Process of qmgr-------------------------------------------" >> $LOG
+ps -ef | grep $qmgr >> $LOG
 
-echo -e "Setting v12 profile -------------------------------------------"
+echo -e "Setting v12 profile -------------------------------------------" >> $LOG
 #setwmb 12
 . /WebSphere/scripts/middleware/wmbprofile 12
 
-echo -e "Migrate qmgr to v12 -------------------------------------------"
-setmqm -m $qmgr -n $v12ver
+echo -e "Migrate qmgr to v12 -------------------------------------------" >> $LOG
+setmqm -m $qmgr -n $v12ver >> $LOG
 
-echo -e "Start the qmgr -------------------------------------------"
-strmqm $qmgr
+echo -e "Start the qmgr -------------------------------------------" >> $LOG
+strmqm $qmgr >> $LOG
 
-echo -e "Display the qmgr -------------------------------------------"
-dspmq -o all | grep $qmgr
+echo -e "Display the qmgr -------------------------------------------" >> $LOG
+dspmq -o all | grep $qmgr >> $LOG
