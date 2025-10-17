ad90e321d32c4f32c130d55e0ab970f7b156d6d0
SudhakarAnemu <sudhakar.anemu@gmail.com>
2025-10-17 15:54:59 -0400

    scriptsscripts

--- Linux/Scripts/Broker/BrkRestartComm_Version.sh
+++
@@ -1,4 +0,0 @@
-#js 
-
-perl /WebSphere/scripts/middleware/wmbRestart.pl IIBT2AA22 stop
-perl /WebSphere/scripts/middleware/wmbRestart.pl IIBT1AB34 start;
--- Linux/Scripts/freeH.sh
+++
@@ -1,9 +0,0 @@
-#js 
-
-#/WebSphere/scripts/middleware/ace/CollFree.sh
-
-LOG=/tmp/free
-dt=`date +"%Y-%m-%d:%H-%M"`
-free=`free -h | grep Mem`
-req=$dt:$free
-echo $req >> $LOG
